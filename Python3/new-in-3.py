976e2fbedf0dedbb22a5e9bd1a43e9aa93286ab8
Ed <eidehua@users.noreply.github.com>
2016-03-09 18:02:17 -0600

    Update new-in-3.py

--- Python3/new-in-3.py
+++ Python3/new-in-3.py
@@ -1,7 +1,12 @@
-@ operator for numpy arrays
+import numpy as np
+import numpy.linalg as la
+
+# @ operator for numpy arrays
 A @ B = np.dot(A,B)
+X = la.solve(A.T @ A, A.T @ b)
 
 # print is a function
 print(A)
 
 # range(0,100) is no longer a list
+
