092f9b2b02c7807ed95a74a228b0d381ec509e9c
Ed <eidehua@users.noreply.github.com>
2016-04-20 22:25:34 -0500

    Update numpy-stumpy.py

--- Python3/numpy-stumpy.py
+++ Python3/numpy-stumpy.py
@@ -1,2 +1,5 @@
+V = np.ones((rows_monte, 2))
+V[:,1] = np.log(h_monte) #second column = log(h)
+b = np.log(monte_carlo_filtered_errors) #b = log(error(h))
 #linear least squares
 monte_EOC = la.solve(V.T @ V, V.T @ b)
