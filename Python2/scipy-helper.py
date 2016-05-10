d36f85f80341ee925cff4ab276b5d30f434a8a03
Ed <eidehua@users.noreply.github.com>
2016-05-09 23:55:15 -0500

    Update scipy-helper.py

--- Python2/scipy-helper.py
+++ Python2/scipy-helper.py
@@ -1,3 +1,10 @@
 
 #broadcasting scalars, so need to supply shape
 D = scipy.sparse.diags([-1/(2*h),0,1/(2*h)], [-1,0,1], shape=(n,n))
+
+  #Scipy special library 
+
+    #grab our weights and nodes
+    ell = sps.legendre(n).weights
+    nodes = ell[:,0]
+    weights = ell[:,1]
